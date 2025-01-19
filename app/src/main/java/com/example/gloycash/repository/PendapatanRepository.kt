f958fdb90a1ac8c22a0d293ee2461793f980c873
defarrel <Defarrel>
2025-01-19 18:28:52 +0700

    Menambahkan PendapatanRepository dan PendapatanService

---
+++ app/src/main/java/com/example/gloycash/repository/PendapatanRepository.kt
@@ -0,0 +1,37 @@
+package com.example.gloycash.repository
+
+import com.example.gloycash.model.AllPendapatanResponse
+import com.example.gloycash.model.Pendapatan
+import com.example.gloycash.service_api.PendapatanService
+
+interface PendapatanRepository {
+    suspend fun insertPendapatan(pendapatan: Pendapatan)
+    suspend fun getPendapatan(): AllPendapatanResponse
+    suspend fun getPendapatanById(id: Int): Pendapatan
+    suspend fun updatePendapatan(id: Int, pendapatan: Pendapatan)
+    suspend fun deletePendapatan(id: Int)
+}
+
+class NetworkPendapatanRepository(
+    private val pendapatanApiService: PendapatanService
+) : PendapatanRepository {
+    override suspend fun insertPendapatan(pendapatan: Pendapatan) {
+        pendapatanApiService.insertPendapatan(pendapatan)
+    }
+
+    override suspend fun getPendapatan(): AllPendapatanResponse {
+        return pendapatanApiService.getPendapatan()
+    }
+
+    override suspend fun getPendapatanById(id: Int): Pendapatan {
+        return pendapatanApiService.getPendapatanById(id).data
+    }
+
+    override suspend fun updatePendapatan(id: Int, pendapatan: Pendapatan) {
+        pendapatanApiService.updatePendapatan(id, pendapatan)
+    }
+
+    override suspend fun deletePendapatan(id: Int) {
+        pendapatanApiService.deletePendapatan(id)
+    }
+}
---
+++ app/src/main/java/com/example/gloycash/service_api/PendapatanService.kt
@@ -0,0 +1,37 @@
+package com.example.gloycash.service_api
+
+import com.example.gloycash.model.AllKategoriResponse
+import com.example.gloycash.model.AllPendapatanResponse
+import com.example.gloycash.model.Kategori
+import com.example.gloycash.model.KategoriDetailResponse
+import com.example.gloycash.model.Pendapatan
+import com.example.gloycash.model.PendapatanDetailResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface PendapatanService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+    @POST("store")
+    suspend fun insertPendapatan(@Body pendapatan: Pendapatan)
+
+    @GET(".")
+    suspend fun getPendapatan(): AllPendapatanResponse
+
+    @GET("{id}")
+    suspend fun getPendapatanById(@Path("id") id: Int): PendapatanDetailResponse
+
+    @PUT("{id}")
+    suspend fun updatePendapatan(@Path("id") id: Int, @Body pendapatan: Pendapatan)
+
+    @DELETE("{id}")
+    suspend fun deletePendapatan(@Path("id") id: Int): Response<Void>
+}
