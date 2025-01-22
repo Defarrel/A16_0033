0f88170d95e48f0df0aae02b207be971deb4c169
defarrel <Defarrel>
2025-01-22 15:02:03 +0700

    Menambahkan getTotalPendapatan di service

--- app/src/main/java/com/example/gloycash/service_api/PendapatanService.kt
+++ app/src/main/java/com/example/gloycash/service_api/PendapatanService.kt
@@ -1,11 +1,9 @@
 package com.example.gloycash.service_api
 
-import com.example.gloycash.model.AllKategoriResponse
 import com.example.gloycash.model.AllPendapatanResponse
-import com.example.gloycash.model.Kategori
-import com.example.gloycash.model.KategoriDetailResponse
 import com.example.gloycash.model.Pendapatan
 import com.example.gloycash.model.PendapatanDetailResponse
+import com.example.gloycash.model.TotalPendapatanResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -26,12 +24,15 @@ interface PendapatanService {
     @GET("pendapatan")
     suspend fun getPendapatan(): AllPendapatanResponse
 
-    @GET("{id}")
+    @GET("pendapatan/{id}")
     suspend fun getPendapatanById(@Path("id") id: Int): PendapatanDetailResponse
 
-    @PUT("{id}")
+    @PUT("pendapatan/{id}")
     suspend fun updatePendapatan(@Path("id") id: Int, @Body pendapatan: Pendapatan)
 
-    @DELETE("{id}")
+    @DELETE("pendapatan/{id}")
     suspend fun deletePendapatan(@Path("id") id: Int): Response<Void>
+
+    @GET("pendapatan/total")
+    suspend fun getTotalPendapatan(): TotalPendapatanResponse
 }
